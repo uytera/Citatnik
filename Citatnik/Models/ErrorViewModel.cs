ab9d1664defee029baf2bafc62e209137fbd2b06
uytera <uytera12@yandex.ru>
2020-03-25 14:18:26 +0500

    Add project files.

---
+++ Citatnik/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Citatnik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
